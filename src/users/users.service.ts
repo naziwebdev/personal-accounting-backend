cffff85da1b32bd28e240c4740d408deb6563b24
nazi <nazipaeez777@gmail.com>
2025-04-22 00:00:21 +0330

    Develope find user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findByPhone(phone: string) {
+    const user = await this.usersRepository.findOne({
+      where: { phone },
+    });
+
+    return user;
+  }
+
+  async findById(id: number) {
+    const user = await this.usersRepository.findOne({
+      where: { id },
+    });
+
+    return user;
+  }
+}
