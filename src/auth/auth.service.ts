7c95970ad35a97c6f7812bded6a756d66a350ac4
nazi <nazipaeez777@gmail.com>
2025-04-23 22:58:50 +0330

    Develope srnd-sms service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from 'src/users/users.service';
+import axios from 'axios';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly patternCodeID = 2962;
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async sendSms(phone: string, otp: string) {
+    const patternValues = ['کاربر عزیز', otp];
+    try {
+      const response = await axios.post(
+        'https://portal.amootsms.com/rest/SendWithPattern',
+        {
+          Mobile: phone,
+          PatternCodeID: this.patternCodeID.toString(),
+          PatternValues: patternValues.join(','),
+        },
+        {
+          headers: {
+            Authorization: this.configService.get<string>('OPT_TOKEN'),
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        },
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error sending OTP:', error);
+    }
+  }
+}
