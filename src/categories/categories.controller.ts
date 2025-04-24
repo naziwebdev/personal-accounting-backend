871aa31763c821d6fc10263e36d58942bc83f451
nazi <nazipaeez777@gmail.com>
2025-04-24 23:39:25 +0330

    Develope create-category api

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { JwtAuthGuard } from 'src/guards/auth.guard';
+import { RoleGuard } from 'src/guards/role.gurad';
+import { CreateCategoryDto } from './dtos/create-category.dto';
+import { Response } from 'express';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  async create(@Body() body: CreateCategoryDto, @Res() res: Response) {
+    const newCategory = await this.categoriesService.create(body);
+
+    return res.status(HttpStatus.CREATED).json({
+      data: newCategory,
+      statusCode: HttpStatus.CREATED,
+      message: 'category created successfully',
+    });
+  }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
+import { CreateCategoryDto } from './dtos/create-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private categoriesRepository: Repository<Category>,
+  ) {}
+
+
+  async create(createCategoryDto:CreateCategoryDto){
+
+    const category = await this.categoriesRepository.create(createCategoryDto)
+    return await this.categoriesRepository.save(category)
+  }
+}
