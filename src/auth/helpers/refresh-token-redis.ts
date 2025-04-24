3d8d1bfef0ee8a24b3b57303daabc66698de7520
nazi <nazipaeez777@gmail.com>
2025-04-24 21:27:22 +0330

    Develope refresh-token-redis helpers

---
+++ src/auth/helpers/refresh-token-redis.ts
@@ -0,0 +1,44 @@
+import { Redis } from 'ioredis';
+import * as bcrypt from 'bcrypt';
+
+export const getRefreshTokenRedisPattern = (userId: number) => {
+  return `refreshToken:${userId}`;
+};
+
+export const setRefreshTokenInRedis = async (
+  redisClient: Redis,
+  refreshToken: string,
+  userId: number,
+) => {
+  const hashedRefreshToken = await bcrypt.hash(refreshToken, 12);
+
+  try {
+    await redisClient.set(
+      getRefreshTokenRedisPattern(userId),
+      hashedRefreshToken,
+      'EX',
+      30 * 24 * 60 * 60,
+    );
+  } catch (error) {
+    throw new Error(`Error setting refresh-token in Redis: ${error.message}`);
+  }
+};
+
+export const getRefreshTokenRedis = async (
+  redisClient: Redis,
+  userId: number,
+) => {
+  const pattern = getRefreshTokenRedisPattern(userId);
+  try {
+    const refreshToken = await redisClient.get(pattern);
+    if (!refreshToken) {
+      return {
+        expired: true,
+        refreshToken: null,
+      };
+    }
+    return refreshToken;
+  } catch (error) {
+    throw error;
+  }
+};
