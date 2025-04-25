5d217e188fb2cefec6795b021be52e81ad360e4c
nazi <nazipaeez777@gmail.com>
2025-04-26 00:44:28 +0330

    Develope create-card api

--- src/bank-cards/bank-cards.controller.ts
+++ src/bank-cards/bank-cards.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { BankCardsService } from './bank-cards.service';
+import { CreateCardDto } from './dtos/create-card.dto';
+import { getUser } from 'src/decorators/get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { Response } from 'express';
+import { JwtAuthGuard } from 'src/guards/auth.guard';
 
-@Controller('bank-cards')
+@Controller('cards')
 export class BankCardsController {
   constructor(private readonly bankCardsService: BankCardsService) {}
+
+  @Post('/')
+  @UseGuards(JwtAuthGuard)
+  async create(
+    @getUser() user: User,
+    @Body() body: CreateCardDto,
+    @Res() res: Response,
+  ) {
+    const card = await this.bankCardsService.create(body, user);
+
+    return res.status(HttpStatus.CREATED).json({
+      data: card,
+      statusCode: HttpStatus.CREATED,
+      message: 'card created successfully',
+    });
+  }
 }
--- src/bank-cards/bank-cards.service.ts
+++ src/bank-cards/bank-cards.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { BankCards } from './entities/bank-card.entity';
+import { CreateCardDto } from './dtos/create-card.dto';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
-export class BankCardsService {}
+export class BankCardsService {
+  constructor(
+    @InjectRepository(BankCards)
+    private bankCardsRepository: Repository<BankCards>,
+  ) {}
+
+  async create(createCardDto: CreateCardDto, user: User) {
+    const bankCard = await this.bankCardsRepository.create({
+      ...createCardDto,
+      user,
+    });
+
+    return await this.bankCardsRepository.save(bankCard);
+  }
+}
