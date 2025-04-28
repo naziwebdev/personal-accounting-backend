93ee5ab426918c13b7c27f1639377608cf6eee1b
nazi <nazipaeez777@gmail.com>
2025-04-28 23:37:03 +0330

    Develope create-note-dto

---
+++ src/notes/dtos/create-note.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, Length, IsNotEmpty } from 'class-validator';
+
+export class createNoteDto {
+  @IsString({ message: 'title must be string' })
+  @IsNotEmpty({ message: 'title is required' })
+  @Length(3, 150, { message: 'title must have 3-150 length' })
+  title: string;
+
+  @IsString({ message: 'description must be string' })
+  @IsNotEmpty({ message: 'description is required' })
+  @Length(3, 2500, { message: 'description must have 3-150 length' })
+  description: string;
+}
