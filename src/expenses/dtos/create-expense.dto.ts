d61ceefeb25ddf349b6bc052719f286433bc1384
nazi <nazipaeez777@gmail.com>
2025-04-28 22:54:55 +0330

    Develope expense dtos

---
+++ src/expenses/dtos/create-expense.dto.ts
@@ -0,0 +1,36 @@
+import {
+    IsString,
+    IsNumber,
+    IsNotEmpty,
+    IsOptional,
+    Length,
+    IsInt,
+    IsDateString,
+  } from 'class-validator';
+  
+  export class CreateExpenseDto {
+    @IsString({ message: 'title must be string' })
+    @Length(3, 50, { message: 'min length 3 and max length is 50' })
+    @IsNotEmpty({ message: 'title is required' })
+    title: string;
+  
+    @IsNumber()
+    @IsNotEmpty({ message: 'price is required' })
+    price: number;
+  
+    @IsDateString()
+    @IsNotEmpty({ message: 'date is required' })
+    date: string; 
+  
+    @IsOptional()
+    description: string;
+  
+    @IsInt()
+    @IsNotEmpty({ message: 'category id is required' })
+    category_id: number;
+  
+    @IsInt()
+    @IsOptional()
+    bankCard_id: number;
+  }
+  
