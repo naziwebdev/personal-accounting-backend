5f8d362fc8bbd13cd9c097ae192bd8831076d151
nazi <nazipaeez777@gmail.com>
2025-04-28 23:45:35 +0330

    Develope create-note api

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpStatus,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { NotesService } from './notes.service';
+import { JwtAuthGuard } from 'src/guards/auth.guard';
+import { getUser } from 'src/decorators/get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { CreateNoteDto } from './dtos/create-note.dto';
+import { Response } from 'express';
 
 @Controller('notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
+
+  @Post('/')
+  @UseGuards(JwtAuthGuard)
+  async create(
+    @getUser() user: User,
+    @Body() body: CreateNoteDto,
+    @Res() res: Response,
+  ) {
+    const note = await this.notesService.create(body, user);
+
+    return res.status(HttpStatus.CREATED).json({
+      data: note,
+      statusCode: HttpStatus.CREATED,
+      message: 'note created successfully',
+    });
+  }
 }
--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Note } from './entities/note.entity';
+import { Repository } from 'typeorm';
+import { CreateNoteDto } from './dtos/create-note.dto';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
-export class NotesService {}
+export class NotesService {
+  constructor(
+    @InjectRepository(Note)
+    private notesRepository: Repository<Note>,
+  ) {}
+
+  async create(createNoteDto: CreateNoteDto, user: User) {
+    const note = await this.notesRepository.create({
+      ...createNoteDto,
+      user,
+    });
+
+    return await this.notesRepository.save(note)
+  }
+}
