d1814f6d6be7c39fb0254282eb8397c605161f5e
nazi <nazipaeez777@gmail.com>
2025-04-30 23:37:04 +0330

    Develope update-check-dto

---
+++ src/checks/dtos/update-check.dto.ts
@@ -0,0 +1,43 @@
+import {
+  IsString,
+  IsOptional,
+  IsNumber,
+  IsEnum,
+  IsDateString,
+} from 'class-validator';
+import { CheckTypeEnum } from '../enums/check-type-enum';
+
+export class CreateCheckDto {
+  @IsEnum(CheckTypeEnum, { message: 'type must be pay or receive' })
+  @IsOptional()
+  @IsString()
+  type: CheckTypeEnum;
+
+  @IsNumber()
+  @IsOptional()
+  price: number;
+
+  @IsString({ message: 'bank must be string' })
+  @IsOptional()
+  bank: string;
+
+  @IsString({ message: 'payable must be string' })
+  @IsOptional()
+  payable: string;
+
+  @IsDateString()
+  @IsOptional()
+  issued: string;
+
+  @IsDateString()
+  @IsOptional()
+  due_date: string;
+
+  @IsOptional()
+  @IsString()
+  serial: string;
+
+  @IsOptional()
+  @IsString()
+  description: string;
+}
