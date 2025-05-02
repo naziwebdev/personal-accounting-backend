dd0b92eca52a6c65d2c101a1ce980d58dca06c2f
nazi <nazipaeez777@gmail.com>
2025-05-02 20:54:30 +0330

    Develope create-loan-dto

---
+++ src/loans/dtos/create-loan.dto.ts
@@ -0,0 +1,45 @@
+import {
+  IsDateString,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
+import { LoanPeriodEnum } from '../enums/loan-period-enum';
+
+export class CreateLoanDto {
+  @IsString()
+  @IsNotEmpty({ message: 'giver name is required' })
+  @Length(2, 50, { message: 'giver name length must be btw 2-50' })
+  giverName: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'giver name is required' })
+  @Length(2, 50, { message: 'giver name length must be btw 2-50' })
+  title: string;
+
+  @IsNumber()
+  @IsNotEmpty({ message: 'total price is required' })
+  totalPrice: number;
+
+  @IsString()
+  @IsOptional()
+  @Length(3, 2000, { message: 'giver name length must be btw 2-50' })
+  description: string;
+
+  @IsInt()
+  @IsNotEmpty({ message: 'count of installment is required' })
+  countInstallment: number;
+
+  @IsDateString()
+  @IsNotEmpty({ message: 'count of installment is required' })
+  firstDateInstallment: string;
+
+  @IsEnum(LoanPeriodEnum, {
+    message: 'period of installment must be weekly/monthly,yearly',
+  })
+  periodInstallment: LoanPeriodEnum;
+}
