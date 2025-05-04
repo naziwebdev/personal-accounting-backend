3d4037b88e22c2e67fcfe4351b1098a3c7186706
nazi <nazipaeez777@gmail.com>
2025-05-04 22:14:15 +0330

    Develope update-watchlist-dto

---
+++ src/watchlist/dtos/update-watchlist.dto.ts
@@ -0,0 +1,3 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateWatchlistDto } from './create-watchlist.dto';
+export class UpdateWatchlistDto extends PartialType(CreateWatchlistDto) {}
